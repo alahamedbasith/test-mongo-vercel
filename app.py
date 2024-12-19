5f37169c69a0d8fb4e399c4dc3cd514f73cef08b
Ahamed Basith <81899645+alahamedbasith@users.noreply.github.com>
2024-12-19 13:53:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 import motor.motor_asyncio  # Use motor for async MongoDB operations
-from fastapi import HTTPException
-
 from fastapi.middleware.cors import CORSMiddleware
-
 import os
 from dotenv import load_dotenv
 
@@ -13,7 +10,6 @@ load_dotenv()
 
 # Load MongoDB connection string from environment variable
 MONGODB_URI = os.getenv("MONGODB_URI")
-
 
 # MongoDB setup
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
@@ -23,23 +19,20 @@ try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e)
+    print(f"Could not connect to MongoDB: {e}")
 
 db = client.portfolio
 collection = db.content
 
 app = FastAPI()
 
-from fastapi.middleware.cors import CORSMiddleware
-
+# CORS middleware setup
 app.add_middleware(
     CORSMiddleware,
-    #allow_origins=["http://<frontend-server>"],  # Replace with the URL of your frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 class Content(BaseModel):
     html_content: str
@@ -47,12 +40,15 @@ class Content(BaseModel):
 @app.post("/update_content/")
 async def update_content(html_content: str = Form(...)):
     # Save content to MongoDB
-    await collection.update_one(
-        {"_id": "content"},
-        {"$set": {"html_content": html_content}},
-        upsert=True
-    )
-    return {"message": "Content updated successfully"}
+    try:
+        await collection.update_one(
+            {"_id": "content"},
+            {"$set": {"html_content": html_content}},
+            upsert=True
+        )
+        return {"message": "Content updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error updating content: {e}")
 
 @app.get("/portfolio", response_class=HTMLResponse)
 async def portfolio():
